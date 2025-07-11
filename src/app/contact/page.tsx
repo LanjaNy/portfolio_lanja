e14c93de18fa712ff7be308f1c25fae0f0209bef
LanjaNy <rnflanja06@gmail.com>
2025-07-11 11:59:09 +0300

    fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -36,7 +36,7 @@ const page = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none ">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent text-center">Let's work together</h3>
+              <h3 className="text-4xl text-accent text-center">Let&apos;s work together</h3>
               <p className="text-white/60">Feel free to reach out if you have any questions, project inquiries, or just want to connect. I’m always open to discussing new opportunities and collaborating on exciting projects!</p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="firstname" placeholder="Firstname"/>
