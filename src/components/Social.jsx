457c76bb25f13583afe043d8b00fd418548d1efe
LanjaNy <rnflanja06@gmail.com>
2025-11-05 11:53:20 +0300

    up fix: add link to my social network

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 import { FaGithub, FaGitlab, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub/>, path: ""},
-    {icon: <FaGitlab/>, path: ""},
-    {icon: <FaLinkedinIn/>, path: ""},
-    {icon: <FaWhatsapp/>, path: ""},
+    {icon: <FaGithub/>, path: "https://github.com/LanjaNy"},
+    {icon: <FaGitlab/>, path: "https://gitlab.com/NyLanja"},
+    {icon: <FaLinkedinIn/>, path: "https://mg.linkedin.com/in/ny-fahafahana-lanja-randriamanantena-8234a7229"},
+    {icon: <FaWhatsapp/>, path: "https://wa.me/+261341415187"},
 ]
 const Social = ({containerStyles, iconStyles}) => {
   return (
     <div className={containerStyles}>
         {socials.map((item, index) => {
-            return <Link key={index} href={item.path} className={iconStyles}>{item.icon}</Link>
+            return <Link key={index} href={item.path} target="_blank" className={iconStyles}>{item.icon}</Link>
         })}
     </div>
   )
